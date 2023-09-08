2a9c9546a2489aed0d5fc44038eabd4f6720ff01
Deyvi Condori Chuquipa <N00378436@upn.pe>
2023-09-08 16:30:11 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EcommerceDiscount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el total de la compra:");
+            decimal monto = Convert.ToDecimal(Console.ReadLine());
+
+            if (monto > 1000m)
+            {
+                decimal descuento = monto * 0.20m;
+                monto -= descuento;
+                Console.WriteLine($"Se ha aplicado un 20% de descuento. Monto con descuento: ${monto}");
+            }
+            else if (monto > 500m)
+            {
+                decimal descuento = monto * 0.10m;
+                monto -= descuento;
+                Console.WriteLine($"Se ha aplicado un 10% de descuento. Monto con descuento: ${monto}");
+            }
+            else
+            {
+                Console.WriteLine($"No se aplica descuento. Monto total: ${monto}");
+            }
+        }
+    }
+}
